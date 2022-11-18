d44989163fbed141bfa115914a5f0277960020fd
Rajat Saini <103591089+rajatlinux1@users.noreply.github.com>
2022-11-18 13:22:00 +0530

    Update files_organiser.py

--- files_organiser.py
+++ files_organiser.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-import os, glob, shutil
+import os, shutil
 from pathlib import Path
 
 BASE_DIR = str(Path(__file__).resolve())
-list_1 = os.listdir()
-for i in list_1:
+
+for i in os.listdir():
     if os.path.isfile(i):
+        file_path = Path(i)
         try:
-            extensions = i.split(".")[1]
+            extensions = str(file_path.suffix).replace(".","")
             if not os.path.exists(extensions):
                 os.makedirs(extensions)
-            for file in glob.glob(f"*.{extensions}"):
-                if file == BASE_DIR.split("/")[-1]:
-                    continue
-                else:
-                    shutil.move(file, extensions)
+            if i == BASE_DIR.split("/")[-1]:
+                continue
+            shutil.move(i, extensions)
+
         except IndexError:
             extensions="NoExtension"
             if not os.path.exists(extensions):
@@ -23,6 +23,4 @@ for i in list_1:
         except:
             continue
 
-    else:
-        continue
 print("****Files organized successfully****")
