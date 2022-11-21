1969f4b2c545d37674bbe313cdcf7c307fce924e
Rajat Saini <103591089+rajatlinux1@users.noreply.github.com>
2022-11-21 13:48:07 +0530

    Update files_organiser.py

--- files_organiser.py
+++ files_organiser.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-import os, shutil
+import os
+import shutil
 from pathlib import Path
+from datetime import datetime
+from plyer import notification
+
+current_time = datetime.now().strftime('%I:%M %p')
 
 BASE_DIR = str(Path(__file__).resolve())
+
 
 for i in os.listdir():
     if os.path.isfile(i):
         file_path = Path(i)
         try:
-            extensions = str(file_path.suffix).replace(".","")
+            extensions = str(file_path.suffix).replace(".", "")
             if not os.path.exists(extensions):
                 os.makedirs(extensions)
             if i == BASE_DIR.split("/")[-1]:
@@ -16,11 +22,15 @@ for i in os.listdir():
             shutil.move(i, extensions)
 
         except IndexError:
-            extensions="NoExtension"
+            extensions = "NoExtension"
             if not os.path.exists(extensions):
                 os.makedirs(extensions)
             shutil.move(i, extensions)
         except:
             continue
-
-print("****Files organized successfully****")
+notification.notify(
+    title=f"Files Organiser {current_time}",
+    message="Files organized successfully",
+    app_icon="bell-icon_34488.ico",
+    timeout=5000
+)
