a38f3b76a1c4ac0025ccb6cfb0bc8ea0ab743f6a
Rajat Saini <103591089+rajatlinux1@users.noreply.github.com>
2022-11-27 14:35:16 +0530

    Update files_organiser.py

--- files_organiser.py
+++ files_organiser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import os, sys
+import os
+import sys
 import shutil
 from pathlib import Path
 from datetime import datetime
@@ -10,33 +11,38 @@ current_time = datetime.now().strftime('%I:%M %p')
 BASE_DIR = str(Path(__file__).resolve())
 
 
-for i in os.listdir():
-    if os.path.isfile(i):
-        file_path = Path(i)
+for file in os.listdir():
+
+    if os.path.isfile(file) and not file.startswith('.'):
+        file_path = Path(file)
+
         try:
             extensions = str(file_path.suffix).replace(".", "")
             if not os.path.exists(extensions):
-                os.makedirs(extensions)
-            if i == BASE_DIR.split("/")[-1] or i==sys.prefix.split("/")[-1] or i==sys.prefix.split("\\")[-1] or i=="icon.ico": #To ignore this files or directories
+                os.mkdir(extensions)
+            # To ignore this files or directories
+            if file == BASE_DIR.split("/")[-1] or file == sys.prefix.split("/")[-1] or file == sys.prefix.split("\\")[-1]:
                 continue
-            shutil.move(i, extensions)
+            shutil.move(file, extensions)
 
         except IndexError:
             extensions = "NoExtension"
             if not os.path.exists(extensions):
                 os.makedirs(extensions)
-            shutil.move(i, extensions)
+            shutil.move(file, extensions)
+
         except Exception as e:
             notification.notify(
-            title=f"Files Organiser {current_time}",
-            message=e,
-            # app_icon="icon.ico",
-            timeout=5000
-        )
-            continue
-notification.notify(
-    title=f"Files Organiser {current_time}",
-    message="Files organized successfully",
-    # app_icon="icon.ico",
-    timeout=5000
-)
+                title=f"Files Organizer {current_time}",
+                message=e,
+                # app_icon="icon.ico",
+                timeout=5000
+            )
+
+if not Exception:
+    notification.notify(
+        title=f"Files Organizer {current_time}",
+        message="Files organized successfully",
+        # app_icon="icon.ico",
+        timeout=5000
+    )
